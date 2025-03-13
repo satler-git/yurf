a5772f7c4c330f4fdab099e8a0cfd62224372552
satler <satler@satler.dev>
2025-03-13 18:47:33 +0900

    feat: implement dummy ui

--- flake.nix
+++ flake.nix
@@ -1,21 +1,11 @@
 {
-  description = "A full Rust flake";
+  description = "A very basic flake";
 
   inputs = {
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    git-hooks-nix.url = "github:cachix/git-hooks.nix";
-
-    systems.url = "github:nix-systems/default";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
     crane.url = "github:ipetkov/crane";
   };
@@ -24,109 +14,53 @@
     inputs@{
       self,
       nixpkgs,
-      flake-parts,
       crane,
       ...
     }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
+    let
+      system = "x86_64-linux";
 
-      imports = [
-        inputs.treefmt-nix.flakeModule
-        inputs.git-hooks-nix.flakeModule
-      ];
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          inputs.rust-overlay.overlays.default
+        ];
+      };
 
-      perSystem =
-        {
-          config,
-          system,
-          pkgs,
-          lib,
-          ...
-        }:
-        let
-          rust-bin = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-          craneLib = (crane.mkLib pkgs).overrideToolchain rust-bin;
+      rust-bin = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-          commonArgs = {
-            src = craneLib.cleanCargoSource ./.;
-            strictDeps = true;
+      craneLib = (crane.mkLib pkgs).overrideToolchain rust-bin;
+      src = craneLib.cleanCargoSource ./.;
 
-            buildInputs = with pkgs; [ ];
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
 
-            nativeBuildInputs = with pkgs; [ ];
-          };
+        # buildInputs = [ ];
+      };
 
-          cargoArtifacts = craneLib.buildDepsOnly (
-            commonArgs
-            // {
-              pname = "deps";
-            }
-          );
-        in
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              inputs.rust-overlay.overlays.default
-            ];
-          };
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-          treefmt = {
-            projectRootFile = "flake.nix";
+      yurf = craneLib.buildPackage (
+        commonArgs
+        // {
+          inherit cargoArtifacts;
+        }
+      );
+    in
+    {
+      packages = {
+        default = yurf;
+      };
 
-            programs = {
-              nixfmt.enable = true;
-              rustfmt = {
-                enable = true;
-                package = rust-bin;
-              };
-              actionlint.enable = true;
-            };
-          };
+      devShells.${system}.default = pkgs.mkShell {
+        name = "ltrait";
 
-          pre-commit = {
-            settings = {
-              hooks = {
-                flake-treefmt = {
-                  enable = true;
-                  name = "flake-treefmt";
-                  entry = lib.getExe config.treefmt.build.wrapper;
-                  pass_filenames = false;
-                };
+        buildInputs = with pkgs; [
+          rust-bin
 
-                # clippy.enable = true;
-                cargo-check.enable = true;
-
-                clippy.packageOverrides.cargo = rust-bin;
-                clippy.packageOverrides.clippy = rust-bin;
-              };
-
-              settings.rust.check.cargoDeps = pkgs.rustPlatform.importCargoLock {
-                lockFile = ./Cargo.lock;
-              };
-            };
-          };
-
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [ config.pre-commit.devShell ];
-
-            buildInputs = with pkgs; [
-              cargo-expand
-              cargo-nextest
-
-              rust-bin
-            ];
-          };
-
-          packages.default = craneLib.buildPackage (
-            commonArgs
-            // {
-              inherit cargoArtifacts;
-              pname = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.name;
-              version = "unstable-${self.shortRev or "dirty"}";
-            }
-          );
-        };
+          cargo-nextest
+        ];
+      };
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
-fn main() {
-    println!("Hello, world!");
+use ltrait::color_eyre::Result;
+use ltrait::{Launcher, Level};
+use ltrait_source_desktop::DesktopEntry;
+// use ltrait_ui_tui::style::Style;
+// use ltrait_ui_tui::{TuiConfig, TuiEntry};
+
+enum Item {
+    Desktop(DesktopEntry),
+}
+
+struct DummyUI;
+
+use ltrait::ui::{Buffer, UI};
+
+impl<'a> UI<'a> for DummyUI {
+    type Context = ();
+
+    async fn run<Cusion: 'a + Send>(
+        &self,
+        mut batcher: ltrait::launcher::batcher::Batcher<'a, Cusion, Self::Context>,
+    ) -> Result<Cusion> {
+        let mut buf = Buffer::default();
+
+        let mut more = true;
+        while more {
+            more = batcher.merge(&mut buf).await?;
+            eprintln!("{}", buf.len());
+        }
+        batcher.compute_cusion(0)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    ltrait::setup(Level::INFO)?;
+
+    let launcher = Launcher::default()
+        .add_source(ltrait_source_desktop::new()?, |e| {
+            eprintln!("{}", e.entry.appid);
+            Item::Desktop(e)
+        })
+        // .batch_size(50)
+        .batch_size(0)
+        .set_ui(DummyUI, |_| ());
+    // .batch_size(50)
+    // .set_ui(
+    //     ltrait_ui_tui::Tui::new(TuiConfig::new(8, '>', ' ')),
+    //     |cusion| match cusion {
+    //         Item::Desktop(entry) => {
+    //             let entry = &entry.entry;
+    //             TuiEntry {
+    //                 text: (
+    //                     entry
+    //                         .name(&["ja", "en"])
+    //                         .or_else(|| Some(entry.id().into()))
+    //                         .unwrap()
+    //                         .to_string(),
+    //                     Style::default(),
+    //                 ),
+    //             }
+    //         }
+    //     },
+    // );
+
+    launcher.run().await?;
+
+    Ok(())
 }
