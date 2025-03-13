4c6f7b8b880415394a51b236de087c20c1a2cac1
satler <satler@satler.dev>
2025-03-13 18:47:33 +0900

    chore: update

--- flake.nix
+++ flake.nix
@@ -1,35 +1,132 @@
 {
-  description = "A very basic flake";
+  description = "A full Rust flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+
+    systems.url = "github:nix-systems/default";
+
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs =
-    inputs@{ self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      crane,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          inputs.rust-overlay.overlays.default
-        ];
-      };
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.git-hooks-nix.flakeModule
+      ];
 
-      rust-bin = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        name = "ltrait";
+      perSystem =
+        {
+          config,
+          system,
+          pkgs,
+          lib,
+          ...
+        }:
+        let
+          rust-bin = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          craneLib = (crane.mkLib pkgs).overrideToolchain rust-bin;
 
-        buildInputs = with pkgs; [
-          rust-bin
+          commonArgs = {
+            src = craneLib.cleanCargoSource ./.;
+            strictDeps = true;
 
-          cargo-nextest
-        ];
-      };
+            buildInputs = with pkgs; [ ];
+
+            nativeBuildInputs = with pkgs; [ ];
+          };
+
+          cargoArtifacts = craneLib.buildDepsOnly (
+            commonArgs
+            // {
+              pname = "deps";
+            }
+          );
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+            ];
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+
+            programs = {
+              nixfmt.enable = true;
+              rustfmt = {
+                enable = true;
+                package = rust-bin;
+              };
+              actionlint.enable = true;
+            };
+          };
+
+          pre-commit = {
+            settings = {
+              hooks = {
+                flake-treefmt = {
+                  enable = true;
+                  name = "flake-treefmt";
+                  entry = lib.getExe config.treefmt.build.wrapper;
+                  pass_filenames = false;
+                };
+
+                # clippy.enable = true;
+                cargo-check.enable = true;
+
+                clippy.packageOverrides.cargo = rust-bin;
+                clippy.packageOverrides.clippy = rust-bin;
+              };
+
+              settings.rust.check.cargoDeps = pkgs.rustPlatform.importCargoLock {
+                lockFile = ./Cargo.lock;
+              };
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ config.pre-commit.devShell ];
+
+            buildInputs = with pkgs; [
+              cargo-expand
+              cargo-nextest
+
+              rust-bin
+            ];
+          };
+
+          packages.default = craneLib.buildPackage (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              pname = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.name;
+              version = "unstable-${self.shortRev or "dirty"}";
+            }
+          );
+        };
     };
 }
