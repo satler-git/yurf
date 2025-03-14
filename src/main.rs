bcf8701962d2789fb483e9f2f2f9d4e4efe616bb
satler <satler@satler.dev>
2025-03-14 11:34:32 +0900

    feat: さらにいろいろ実験

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
 use ltrait::action::ClosureAction;
 use ltrait::color_eyre::Result;
-use ltrait::filter::ClosureFilter;
-use ltrait::generator::ClosureGenerator;
 use ltrait::{Launcher, Level};
 use ltrait_gen_calc::{Calc, CalcConfig};
 use ltrait_scorer_nucleo::{CaseMatching, Context};
 use ltrait_source_desktop::DesktopEntry;
 // use ltrait_ui_tui::style::Style;
 // use ltrait_ui_tui::{TuiConfig, TuiEntry};
+use ltrait_extra::sorter::{ReversedSorter, SorterIf};
 
 enum Item {
     Desktop(DesktopEntry),
     Num(u32),
     Calc(String),
+}
+
+impl Into<String> for &Item {
+    fn into(self) -> String {
+        match self {
+            Item::Desktop(e) => e
+                .entry
+                .name(&["ja", "en"])
+                .or_else(|| Some(e.entry.id().into()))
+                .unwrap()
+                .into(),
+            Item::Num(e) => format!("{e}"),
+            Item::Calc(s) => s.into(),
+        }
+    }
 }
 
 struct DummyUI;
@@ -29,14 +43,14 @@ impl<'a> UI<'a> for DummyUI {
         let mut buf = Buffer::default();
 
         let mut more = true;
-        batcher.input(&mut buf, "= 1 + 2".into());
+        batcher.input(&mut buf, "firefox".into());
         while more {
             more = batcher.merge(&mut buf).await?;
             eprintln!("{}", buf.len());
         }
-        // while let Some(s) = buf.next() {
-        //     eprintln!("{}", s.0);
-        // }
+        while let Some(s) = buf.next() {
+            eprintln!("{}", s.0);
+        }
         batcher.compute_cusion(0)
     }
 }
@@ -48,58 +62,48 @@ async fn main() -> Result<()> {
     let launcher = Launcher::default()
         // .add_source(ltrait_source_desktop::new()?, Item::Desktop) // TODO: 遅い
         .add_source(Box::pin(ltrait::tokio_stream::iter(1..=5000)), Item::Num)
-        .add_generator(
-            ClosureGenerator::new(|input| vec![input.to_string()]),
-            Item::Calc,
-        )
         // .add_generator(
-        //     Calc::new(CalcConfig::new(
-        //         (Some('k'), None),
-        //         None,
-        //         Some(52), // 精度
-        //         None,
-        //     )),
+        //     ClosureGenerator::new(|input| vec![input.to_string()]),
         //     Item::Calc,
         // )
+        .add_generator(
+            Calc::new(CalcConfig::new(
+                (Some('k'), None),
+                None,
+                None, // 精度
+                None,
+            )),
+            Item::Calc,
+        )
         .add_sorter(
-            ltrait_scorer_nucleo::NucleoMatcher::new(
-                // TODO: reverse
-                false,
-                CaseMatching::Smart,
-                ltrait_scorer_nucleo::Normalization::Smart,
-            )
-            .into_sorter(),
-            |c| Context {
-                match_string: match c {
-                    Item::Desktop(e) => e
-                        .entry
-                        .name(&["ja", "en"])
-                        .or_else(|| Some(e.entry.id().into()))
-                        .unwrap()
-                        .into(),
-                    Item::Num(e) => format!("{e}"),
-                    Item::Calc(s) => s.clone(),
+            ReversedSorter::new(SorterIf::new(
+                ltrait_scorer_nucleo::NucleoMatcher::new(
+                    false,
+                    CaseMatching::Smart,
+                    ltrait_scorer_nucleo::Normalization::Smart,
+                )
+                .into_sorter(),
+                |c: &Context| &c.match_string != "",
+            )),
+            |c| match c {
+                Item::Desktop(_) => Context {
+                    match_string: c.into(),
+                },
+                _ => Context {
+                    match_string: "".into(),
                 },
             },
         )
         // .add_filter(ClosureFilter::new(|_, _| false), |_| ())
         // .batch_size(50)
         .batch_size(0)
-        .set_ui(DummyUI, |c| match c {
-            Item::Desktop(e) => e.entry.appid.clone().into(),
-            Item::Num(e) => format!("{e}"),
-            Item::Calc(s) => s.clone(),
-        })
+        .set_ui(DummyUI, |c| c.into())
         .add_action(
-            ClosureAction::new(|s| {
+            ClosureAction::new(|s: &String| {
                 println!("{s}");
                 Ok(())
             }),
-            |c| match c {
-                Item::Desktop(e) => e.entry.appid.clone().into(),
-                Item::Num(e) => format!("{e}"),
-                Item::Calc(s) => s.clone(),
-            },
+            |c| c.into(),
         );
     // .batch_size(50)
     // .set_ui(
